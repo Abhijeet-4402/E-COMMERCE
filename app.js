6c8a5c055b29481b9e3fea679c3e4bf54cec0802
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-09-05 01:12:40 +0530

    day-2

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-const path =require('path');
+const path = require('path');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const seedDB = require('./seed');
 const productRoutes = require('./routes/product');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override')
+
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/shopping-sam-app')
-.then(()=>{
-    console.log("DB connected successfully");
-})
-.catch((err)=>{
-    console.log("ERROR => ",err);
-})
+    .then(() => {
+        console.log("DB connected successfully");
+    })
+    .catch((err) => {
+        console.log("ERROR => ", err);
+    })
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');  // change view engine to ejs
-app.set('views', path.join(__dirname,'views'));  // connect views folder
-app.set(express.static(path.join(__dirname,'public')));  // connect public folder
+app.set('views', path.join(__dirname, 'views'));  // connect views folder
+app.set(express.static(path.join(__dirname, 'public')));  // connect public folder
 
 //Seeding DataBase: 
 // seedDB();
 
+app.use(express.urlencoded({extended : true})); // middleWare for parsing incoming request.
+app.use(methodOverride('_method')); // for overriding form method like put patch delete
+
+//Saare middlewares iske phle use krna hai
 app.use(productRoutes); // So that har incoming request ke liye path check kiya jaye.
 
-app.listen(8080, ()=>{
+
+app.listen(8080, () => {
     console.log("Server Connected at port 8080");
 })
--- seed.js
+++ seed.js
@@ -5,39 +5,39 @@ const products = [
     {
         name: 'I-Phone 14 Pro',
         img: 'https://images.unsplash.com/photo-1678685888221-cda773a3dcdb?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        price:130000,
+        price: 130000,
         desc: 'Very costly, Aukaat ke bahar'
     },
     {
-        name:'MacBook M2 Pro',
+        name: 'MacBook M2 Pro',
         img: 'https://images.unsplash.com/photo-1569770218135-bea267ed7e84?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         price: 250000,
         desc: 'ye to bilkul hi aukaat ke bahar hai '
     },
     {
-        name:'Apple Watch',
+        name: 'Apple Watch',
         img: 'https://images.unsplash.com/photo-1553103291-a40940bedc81?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         price: 51000,
         desc: ' ye sasta hai le lo'
     },
     {
-        name:'I-Pad pro',
+        name: 'I-Pad pro',
         img: 'https://images.unsplash.com/photo-1661340272675-f6829791246e?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         price: 237000,
         desc: 'Rhne de nhi le paayega'
     },
     {
-        name:'Airpods Pro',
+        name: 'Airpods Pro',
         img: 'https://images.unsplash.com/photo-1587523459887-e669248cf666?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         price: 25000,
         desc: 'Kuch chije sirf dekhne ke liye hoti hai'
     }
 ];
 
-async function seedDB(){
+async function seedDB() {
     await Product.insertMany(products);
     console.log('Data Seeded Successfully');
-    
+
 }
 
 module.exports = seedDB;
