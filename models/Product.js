366bdbe0c8aac6c6569ddaa1991937d936e524b8
Abhijeet-4402 <abhirajpoot4402@gmail.com>
2025-09-07 01:48:50 +0530

    Day-3

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 const mongoose  = require("mongoose");
-
+const Review = require("../models/Review");
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -28,6 +28,16 @@ const productSchema = new mongoose.Schema({
         }
     ]
 })
+
+//middleWare jo behind the scene MongoDB operations krwane pr use hote hai and inke andr pre and post middleWare hote hai
+//jo basically model bnane ke phle schema pr use hote hai.
+
+productSchema.post('findOneAndDelete', async function(product){
+    if(product.reviews.length>0){
+        await Review.deleteMany({_id:{$in:product.reviews}})
+    }
+})
+
 
 let Product = mongoose.model('Product', productSchema);
 
